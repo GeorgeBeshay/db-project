8d6eab3df3bc9d79fe75e7e8b4178269affda59d
Patrick Georges <patrick.georges18@gmail.com>
2023-12-24 16:21:31 +0200

    Created Administrator and SHELTER relations in SQL.

--- src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
+++ src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
@@ -1,3 +1,19 @@
 USE PASMS_DB;
 GO
 
+Create table ADMINISTRATOR(
+admin_id int primary key,
+fname varchar(20) NOT NULL,
+lname varchar(20) not null,
+email varchar(30) unique not null,
+phone varchar(15),
+password_hash varchar(256) not null)
+
+Create table SHELTER(
+shelter_id int primary key,
+shelter_name varchar(50) not null,
+shelter_location varchar(255) not null,
+shelter_email varchar(255) not null,
+shelter_phone varchar(15) not null,
+shelter_manager int foreign key references STAFF(id));
+
