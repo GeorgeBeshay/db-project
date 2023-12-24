44be07e1bf6ff279f99f6e66e5a9defe1c1332dc
bangkook <m.hossam2551@gmail.com>
2023-12-24 20:30:01 +0200

    Utilize george's function for existing tables.

--- src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
+++ src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
@@ -2,37 +2,66 @@ USE PASMS_DB;
 GO
 
 
-CREATE TABLE ADOPTION_APPLICATION (
-	id INT,
-	adopter_id INT NOT NULL,
-	pet_id INT NOT NULL,
-	status TINYINT NOT NULL,
-	marital_status TINYINT NOT NULL,
-	experience BIT NOT NULL,
-	creation_date DATE NOT NULL,
-	closing_date DATE,
-	description VARCHAR(MAX) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id),
-	FOREIGN KEY (pet_id) REFERENCES PET(id)
-);
+-- Guard condition to check for relation absence before creating it.
+DECLARE @inputTableName VARCHAR(128) = 'ADOPTION_APPLICATION';
+IF dbo.relation_exists(@inputTableName) = 0
+BEGIN
+    CREATE TABLE ADOPTION_APPLICATION (
+        id INT,
+        adopter_id INT NOT NULL,
+        pet_id INT NOT NULL,
+        status TINYINT NOT NULL,
+        marital_status TINYINT NOT NULL,
+        experience BIT NOT NULL,
+        creation_date DATE NOT NULL,
+        closing_date DATE,
+        description VARCHAR(MAX) NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id),
+        FOREIGN KEY (pet_id) REFERENCES PET(id)
+    );
+END
+ELSE
+BEGIN
+   PRINT 'ADOPTION_APPLICATION relation already exists.';
+END
+GO
 
-CREATE TABLE APPLICATION_NOTIFICATION (
-	adoption_application_id INT NOT NULL,
-	adopter_id INT NOT NULL,
-	status BIT NOT NULL,
-	date DATE NOT NULL,
-	PRIMARY KEY (adoption_application_id, adopter_id),
-	FOREIGN KEY (adoption_application_id) REFERENCES ADOPTION_APPLICATION(id),
-	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
-);
+-- Guard condition to check for relation absence before creating it.
+DECLARE @inputTableName VARCHAR(128) = 'APPLICATION_NOTIFICATION';
+IF dbo.relation_exists(@inputTableName) = 0
+BEGIN
+    CREATE TABLE APPLICATION_NOTIFICATION (
+        adoption_application_id INT NOT NULL,
+        adopter_id INT NOT NULL,
+        status BIT NOT NULL,
+        date DATE NOT NULL,
+        PRIMARY KEY (adoption_application_id, adopter_id),
+        FOREIGN KEY (adoption_application_id) REFERENCES ADOPTION_APPLICATION(id),
+        FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
+    );
+END
+ELSE
+BEGIN
+    PRINT 'APPLICATION_NOTIFICATION relation already exists.';
+END
+GO
 
-CREATE TABLE PET_AVAILABILITY_NOTIFICATION (
-	pet_id INT NOT NULL,
-	adopter_id INT NOT NULL,
-	status BIT NOT NULL,
-	date DATE NOT NULL,
-	PRIMARY KEY (pet_id, adopter_id),
-	FOREIGN KEY (pet_id) REFERENCES PET(id),
-	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
-);
+-- Guard condition to check for relation absence before creating it.
+DECLARE @inputTableName VARCHAR(128) = 'PET_AVAILABILITY_NOTIFICATION';
+IF dbo.relation_exists(@inputTableName) = 0
+    CREATE TABLE PET_AVAILABILITY_NOTIFICATION (
+        pet_id INT NOT NULL,
+        adopter_id INT NOT NULL,
+        status BIT NOT NULL,
+        date DATE NOT NULL,
+        PRIMARY KEY (pet_id, adopter_id),
+        FOREIGN KEY (pet_id) REFERENCES PET(id),
+        FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
+    );
+END
+ELSE
+BEGIN
+    PRINT 'PET_AVAILABILITY_NOTIFICATION relation already exists.';
+END
+GO
