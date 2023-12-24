1f3e926a4dcd8e08222c89669bd71f1a420548c6
bangkook <m.hossam2551@gmail.com>
2023-12-24 20:28:53 +0200

    Create the relations.

--- src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
+++ src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
@@ -1,3 +1,38 @@
 USE PASMS_DB;
 GO
 
+
+CREATE TABLE ADOPTION_APPLICATION (
+	id INT,
+	adopter_id INT NOT NULL,
+	pet_id INT NOT NULL,
+	status TINYINT NOT NULL,
+	marital_status TINYINT NOT NULL,
+	experience BIT NOT NULL,
+	creation_date DATE NOT NULL,
+	closing_date DATE,
+	description VARCHAR(MAX) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id),
+	FOREIGN KEY (pet_id) REFERENCES PET(id)
+);
+
+CREATE TABLE APPLICATION_NOTIFICATION (
+	adoption_application_id INT NOT NULL,
+	adopter_id INT NOT NULL,
+	status BIT NOT NULL,
+	date DATE NOT NULL,
+	PRIMARY KEY (adoption_application_id, adopter_id),
+	FOREIGN KEY (adoption_application_id) REFERENCES ADOPTION_APPLICATION(id),
+	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
+);
+
+CREATE TABLE PET_AVAILABILITY_NOTIFICATION (
+	pet_id INT NOT NULL,
+	adopter_id INT NOT NULL,
+	status BIT NOT NULL,
+	date DATE NOT NULL,
+	PRIMARY KEY (pet_id, adopter_id),
+	FOREIGN KEY (pet_id) REFERENCES PET(id),
+	FOREIGN KEY (adopter_id) REFERENCES ADOPTER(id)
+);
