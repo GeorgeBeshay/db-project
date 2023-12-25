7c4c7519f9df345ac6b69a1cbcb206e09101e5a3
George Beshay <georgebeshay101@gmail.com>
2023-12-26 00:23:59 +0200

    - Added the IDENTITY(1, 1) constraint on both Adopter, and Staff relations. - Added a NOT NULL constraint on the shelter id within the Staff relation.

--- src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
+++ src/be-src/src/main/java/db_proj_be/SQL/pasms_ddl.sql
@@ -7,7 +7,7 @@ DECLARE @inputTableName VARCHAR(128) = 'ADOPTER';
 IF dbo.relation_exists(@inputTableName) = 0
 BEGIN
     CREATE TABLE ADOPTER (
-        id INT PRIMARY KEY,
+        id INT PRIMARY KEY IDENTITY(1, 1),
         email VARCHAR(255) UNIQUE NOT NULL,         -- candidate key
         password_hash VARCHAR(256) NOT NULL,
         first_name VARCHAR(50) NOT NULL,
@@ -30,14 +30,14 @@ DECLARE @inputTableName VARCHAR(128) = 'STAFF';
 IF dbo.relation_exists(@inputTableName) = 0
 BEGIN
     CREATE TABLE STAFF (
-        id INT PRIMARY KEY,
+        id INT PRIMARY KEY IDENTITY(1, 1),
         first_name VARCHAR(50) NOT NULL,
         last_name VARCHAR(50) NOT NULL,
         role VARCHAR(50) NOT NULL CHECK (role IN ('Manager', 'Member')),
         phone VARCHAR(15),
         email VARCHAR(255) NOT NULL UNIQUE,        -- added not null constraint.
         password_hash VARCHAR(256) NOT NULL,
-        shelter_id INT,
+        shelter_id INT NOT NULL,
         FOREIGN KEY (shelter_id) REFERENCES SHELTER(id),
         -- The SHELTER relation must be defined before creating this relation.
                         );
