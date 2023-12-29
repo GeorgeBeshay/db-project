9b1baf1d09a1bc8d57cc91dbc1748190d795c424
George Beshay <georgebeshay101@gmail.com>
2023-12-29 23:30:55 +0200

    Created the Staff entity class in the FE, in addition to implementing the Staff Management Endpoints.

--- src/fe-src/src/app/Services/admin-services.service.ts
+++ src/fe-src/src/app/Services/admin-services.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {firstValueFrom} from "rxjs";
 import {Admin} from "../Entities/Admin";
+import {Staff} from "../Entities/Staff";
 
 @Injectable({
   providedIn: 'root'
@@ -17,21 +18,30 @@ export class AdminServicesService {
   async adminSignIn(admin: Admin) {
 
     try {
-
       return await firstValueFrom (
         this.http.post<Admin>(`${this.adminEndpointUrl}adminSignIn`, admin, {responseType:'json'})
       );
 
     } catch (error) {
-
-      if(error instanceof HttpErrorResponse)
-        console.error('Bad request');
-      else
-        console.error('Error');
-
+      console.error(error instanceof HttpErrorResponse ? 'Bad request' : 'Error');
       return null
+
     }
 
+  }
+
+  async createStaff(staff: Staff) {
+
+    try {
+      return await firstValueFrom (
+        this.http.post<Staff>(`${this.adminEndpointUrl}createStaff`, staff, {responseType:'json'})
+      );
+
+    } catch (error) {
+      console.error(error instanceof HttpErrorResponse ? 'Bad request' : 'Error');
+      return null;
+
+    }
   }
 
 }
