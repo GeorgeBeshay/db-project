1b29f0b93d71c55c1f98751a44d3d61fe727757a
George Beshay <georgebeshay101@gmail.com>
2023-12-30 01:43:35 +0200

    - Staff Creation FE (As an admin).

--- src/fe-src/src/app/Components/admin/admin.component.ts
+++ src/fe-src/src/app/Components/admin/admin.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormGroup, Validators } from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {AdminServicesService} from "../../Services/admin-services.service";
 import {UtilitiesService} from "../../Services/utilities.service";
+import {Staff} from "../../Entities/Staff";
 
 @Component({
   selector: 'app-admin',
@@ -14,6 +15,7 @@ export class AdminComponent implements OnInit {
   //  ---------------------------- Component Fields ----------------------------
   admin: Admin | null = null;
   signInForm!: FormGroup;
+  createStaffForm!: FormGroup;
   adminService!: AdminServicesService;
   utilitiesService!: UtilitiesService;
   selectedSection!: number;
@@ -24,9 +26,20 @@ export class AdminComponent implements OnInit {
   }
 
   ngOnInit() {
+
     this.signInForm = this.formBuilder.group({
       id: ['', [Validators.required]],
       password: ['', Validators.required]
+    });
+
+    this.createStaffForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      role: ['', Validators.required],
+      phone: [''],
+      email: ['', [Validators.required, Validators.email]],
+      shelterId: ['', Validators.required],
+      staffPassword: ['', [Validators.required, Validators.minLength(4)]]
     });
 
     let tempObj = sessionStorage.getItem("adminObject");
@@ -52,6 +65,30 @@ export class AdminComponent implements OnInit {
       this.selectSection(0);
     } else {
       await this.utilitiesService.sweetAlertFailure("Authentication Failed.")
+    }
+
+  }
+
+  async createStaff() {
+
+    const firstName = this.createStaffForm.get('firstName')?.value;
+    const lastName = this.createStaffForm.get('lastName')?.value;
+    const role = this.createStaffForm.get('role')?.value;
+    const phone = this.createStaffForm.get('phone')?.value;
+    const email = this.createStaffForm.get('email')?.value;
+    const shelterId = this.createStaffForm.get('shelterId')?.value;
+    const password = this.createStaffForm.get('password')?.value;
+
+    // Create a new Staff object
+    const newStaff = new Staff(undefined, firstName, lastName, role, phone, email, password, shelterId);
+
+
+    // delegate the call to the service
+    let apiResult = await this.adminService.createStaff(newStaff);
+    if (apiResult == null) {
+      await this.utilitiesService.sweetAlertFailure("Staff Record Creation Failed.");
+    } else {
+      await this.utilitiesService.sweetAlertSuccess("Staff Record Created Successfully.")
     }
 
   }
