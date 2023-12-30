c4d03f644a99ec86730c00023a23814a32dba5ce
bangkook <m.hossam2551@gmail.com>
2023-12-30 08:05:21 +0200

    Notifications service methods and api

--- src/be-src/src/main/java/db_proj_be/BusinessLogic/Services/AdopterService.java
+++ src/be-src/src/main/java/db_proj_be/BusinessLogic/Services/AdopterService.java
@@ -2,9 +2,13 @@ package db_proj_be.BusinessLogic.Services;
 
 import db_proj_be.BusinessLogic.EntityModels.Adopter;
 import db_proj_be.BusinessLogic.EntityModels.AdoptionApplication;
+import db_proj_be.BusinessLogic.EntityModels.ApplicationNotification;
+import db_proj_be.BusinessLogic.EntityModels.PetAvailabilityNotification;
 import db_proj_be.BusinessLogic.Utilities.Logger;
 import db_proj_be.Database.DAOs.AdopterDAO;
 import db_proj_be.Database.DAOs.AdoptionApplicationDAO;
+import db_proj_be.Database.DAOs.ApplicationNotificationDAO;
+import db_proj_be.Database.DAOs.PetAvailabilityNotificationDAO;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +19,14 @@ public class AdopterService {
 
     private final AdoptionApplicationDAO adoptionApplicationDAO;
     private final AdopterDAO adopterDAO;
+    private final ApplicationNotificationDAO applicationNotificationDAO;
+    private final PetAvailabilityNotificationDAO petAvailabilityNotificationDAO;
 
     public AdopterService(JdbcTemplate jdbcTemplate) {
         this.adoptionApplicationDAO = new AdoptionApplicationDAO(jdbcTemplate);
         this.adopterDAO = new AdopterDAO(jdbcTemplate);
+        this.applicationNotificationDAO = new ApplicationNotificationDAO(jdbcTemplate);
+        this.petAvailabilityNotificationDAO = new PetAvailabilityNotificationDAO(jdbcTemplate);
     }
 
     public int createAdoptionApplication(AdoptionApplication adoptionApplication) {
@@ -48,4 +56,25 @@ public class AdopterService {
         return adopter;
     }
 
+    public List<ApplicationNotification> fetchAppNotifications(int adopterId) {
+        List<ApplicationNotification> applicationNotifications = this.applicationNotificationDAO.findByAdopterId(adopterId);
+
+        if (applicationNotifications != null)
+            Logger.logMsgFrom(this.getClass().getName(), "Successfully fetched app notifications for adopter " + adopterId, 0);
+        else
+            Logger.logMsgFrom(this.getClass().getName(), "Failed to fetch app notifications for adopter " + adopterId, 1);
+
+        return applicationNotifications;
+    }
+
+    public List<PetAvailabilityNotification> fetchPetNotifications(int adopterId) {
+        List<PetAvailabilityNotification> petNotifications = this.petAvailabilityNotificationDAO.findByAdopterId(adopterId);
+
+        if (petNotifications != null)
+            Logger.logMsgFrom(this.getClass().getName(), "Successfully fetched pet notifications for adopter " + adopterId, 0);
+        else
+            Logger.logMsgFrom(this.getClass().getName(), "Failed to fetch pet notifications for adopter " + adopterId, 1);
+
+        return petNotifications;
+    }
 }
