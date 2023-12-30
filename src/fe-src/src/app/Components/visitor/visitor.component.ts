88a3c295c6d6e3c6446cb19c54b7436fbfc4973f
MariamAziz0 <ma27523@gmail.com>
2023-12-30 08:23:28 +0200

    FE for visitor

--- src/fe-src/src/app/Components/visitor/visitor.component.ts
+++ src/fe-src/src/app/Components/visitor/visitor.component.ts
@@ -11,24 +11,75 @@ import { VisitorServicesService } from 'src/app/Services/visitor-services.servic
 })
 export class VisitorComponent implements OnInit{
 
+  pets: any[] | null = [];
+
   visitorService: VisitorServicesService;
+  searchParams: any = {
+    specie: '',
+    breed: '',
+    birthdate: '',
+    gender: '',
+    shelterLocation: ''
+  };
+
+  sorting: any = {
+    neutering: false,
+    house_training: false,
+    vaccination: false
+  };
 
   constructor(private http: HttpClient) {
     this.visitorService = new VisitorServicesService(http);
   }
 
   async ngOnInit() {
-    let criteria = {
-      "specie": "dog",
-      "gender": true
-    };
+    // let criteria = {
+    //   "specie": "dog",
+    //   "gender": true
+    // };
 
-    let shelterLocation = "";
+    // let shelterLocation = "";
 
-    let orderedByColumns = ["neutering"];
+    // let orderedByColumns = ["neutering"];
 
-    let result = await this.visitorService.getSearchedAndSortedPets(criteria, shelterLocation, orderedByColumns);
-    console.log(result);
+    // let result = await this.visitorService.getSearchedAndSortedPets(criteria, shelterLocation, orderedByColumns);
+    // console.log(result);
+  }
+
+  async search() {
+    let criteria: { [key: string]: string | boolean } = {}
+    let shelterLocation = this.searchParams.shelterLocation
+    let orderedByColumns: string[] = [];
+
+    for (let param in this.searchParams) {
+      if (param === 'shelterLocation') {
+        continue;
+      }
+
+      if (param === 'gender') {
+        if (this.searchParams[param] == 'Male') {
+          criteria[param] = true;
+        }
+        else {
+          criteria[param] = false;
+        }
+      } else if (this.searchParams[param]) {
+        criteria[param] = this.searchParams[param];
+      }
+    }
+
+    for (let sortParam in this.sorting) {
+      if (this.sorting[sortParam]) {
+        orderedByColumns.push(sortParam);
+      }
+    }
+
+    console.log("criteria:", criteria);
+    console.log("shelterLocation:", shelterLocation);
+    console.log("orderedByColumns:", orderedByColumns);
+
+    this.pets = await this.visitorService.getSearchedAndSortedPets(criteria, shelterLocation, orderedByColumns);
+    console.log(this.pets);
   }
 
 }
