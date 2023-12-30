e2f94d1c1a0755aba789a64decae15447486f7e4
MariamAziz0 <ma27523@gmail.com>
2023-12-30 02:29:05 +0200

    service for visitor

--- src/fe-src/src/app/Components/visitor/visitor.component.ts
+++ src/fe-src/src/app/Components/visitor/visitor.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UtilitiesService} from "../../Services/utilities.service";
+import { Pet } from '../../Entities/Pet';
+import { HttpClient } from '@angular/common/http';
+import { VisitorServicesService } from 'src/app/Services/visitor-services.service';
 
 @Component({
   selector: 'app-visitor',
   templateUrl: './visitor.component.html',
   styleUrls: ['./visitor.component.css']
 })
-export class VisitorComponent {
+export class VisitorComponent implements OnInit{
+
+  visitorService: VisitorServicesService;
+
+  constructor(private http: HttpClient) {
+    this.visitorService = new VisitorServicesService(http);
+  }
+
+  async ngOnInit() {
+    let criteria = {
+      "specie": "dog",
+      "gender": true
+    };
+
+    let shelterLocation = "";
+
+    let orderedByColumns = ["neutering"];
+
+    let result = await this.visitorService.getSearchedAndSortedPets(criteria, shelterLocation, orderedByColumns);
+    console.log(result);
+  }
 
 }
--- src/fe-src/src/app/Services/visitor-services.service.ts
+++ src/fe-src/src/app/Services/visitor-services.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { Pet } from '../Entities/Pet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisitorServicesService {
 
-  constructor() { }
+  visitorURL = 'http://localhost:8081/pasms-server/visitor-api/';
+
+
+  constructor(private http: HttpClient) { }
+
+  //  ---------------------------- Service Methods ----------------------------
+
+  async getSearchedAndSortedPets(criteria: object, shelterLocation: string, orderedByColumns: string[]) {
+    let requestedMap = {
+      'criteria': criteria,
+      'shelterLocation': shelterLocation,
+      'orderedByColumns': orderedByColumns
+    };
+
+    try {
+      return await firstValueFrom (
+        this.http.post<Pet[]>(this.visitorURL + 'getSearchedAndSortedPets', requestedMap, {responseType:'json'})
+      );
+
+    } catch (error) {
+      if(error instanceof HttpErrorResponse)
+        console.error('Bad request');
+
+      else
+        console.error('Error');
+    }
+    return null;
+  }
 }
